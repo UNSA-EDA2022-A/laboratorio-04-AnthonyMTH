e7b9cd43930a23e6e21c34597f7233aed7ccf31d
AnthonyTH <90292092+AnthonyMTH@users.noreply.github.com>
2022-06-07 18:45:38 -0500

    Update Node.java

--- src/main/java/com/example/project/Node.java
+++ src/main/java/com/example/project/Node.java
@@ -1,6 +1,6 @@
 package com.example.project;
 
-public class Node<T> {
+public class Node<T> implements Comparable<Node<T>>{
     private T value;      // Valor guardado en el nodo
     private Node<T> next; // Referencia para el proximo nodo de la lista
  
@@ -15,4 +15,12 @@ public class Node<T> {
     public Node<T> getNext()  { return next; }
     public void setValue(T v) { value=v; }
     public void setNext(Node<T> n) { next = n; }
+    public String toString() {
+        return this.data.toString();
+    }
+    public int compareTo(Node<T> e) {
+        if(this.getData().equals(e.getData())) 
+            return 0;
+        return -1;
+    }
  }
